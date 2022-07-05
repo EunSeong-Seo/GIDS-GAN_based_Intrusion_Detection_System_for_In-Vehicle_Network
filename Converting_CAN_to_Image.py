86ba76780a00a65faafd6c1c039e5128b67428b2
EunSeong <seoun4612@gmail.com>
2022-07-05 14:45:03 +0900

    comlete can_img files

--- Converting_CAN_to_Image.py
+++ Converting_CAN_to_Image.py
@@ -2,6 +2,15 @@ import numpy as np
 import pandas as pd
 from PIL import Image
 
+"""
+user setting 
+1. write can_dataset_path which you download in reference paper
+2. make folder to save can_image_dataset
+3. run this code
+"""
+
+can_dataset_path = "dataset/Training set/normal_run_data.txt"
+
 
 def one_hot_vector(c):
     # convert element of can_data to one hot vector to select can image position
@@ -55,33 +64,43 @@ def convert_to_can_data_to_one_hot_vector(can):
 
 
 def make_can_image(data):
-    can_image = np.zeros((1, 16))
+    row_size = 64
+    column_size = 16
+
     # set size of image to 16 X ( 3 * 64 )
     # colum is Hexadecimal presentation from CAN ID value
     # row is  3 (CAN ID length) * 64 (optimal row size in reference paper)
-    num = 1
-    for w in range(0, len(data),64):                                   # <== I have to check this part
-        lst = data.at[w:w+64]
-        for i in range(0, len(lst)):
-            add_can = convert_to_can_data_to_one_hot_vector(lst.iloc[i][0])
-            can_image = np.vstack([can_image, add_can])
-            can_image = np.delete(can_image, (0), axis=0)
-            int_can_image = can_image.astype(int)
 
-        image = Image.fromarray(make_can_image(df))
+    can_img_num = 0
+    for w in range(0, len(data), row_size):
+        can_image = np.zeros((1, column_size))
+        lst = data.iloc[w : w + row_size]
+
+        for i in range(0, len(lst)):
+            add_can = convert_to_can_data_to_one_hot_vector(lst.iloc[i])
+            can_image = np.vstack([can_image, add_can])
+        can_image = np.delete(can_image, (0), axis=0)
+
+        # convert (can_img => int type can_img => RGB can_img) to save to png file form
+        int_can_image = can_image.astype(int)
+        image = Image.fromarray(int_can_image)
         image = image.convert("RGB")
-        image.save('can_img_{}.png'.format(num))
-        num+=1
-    return int_can_image
+
+        # save img file
+        image.save("CAN_image_dataset/Training_set/can_img_{}.png".format(can_img_num))
+        can_img_num += 1
+
+    print("making can image complete | the number of igage : ", can_img_num)
 
 
 # test this code to check working well
+
 
 if __name__ == "__main__":
     """load normal_run_data.txt"""
     can_feature = ["Timestamp", "blank", "ID", "zero", "DLC", "Data"]
     data_for_test = pd.read_csv(
-        "dataset/Training set/normal_run_data.txt",
+        can_dataset_path,
         sep="    ",
         engine="python",
         encoding="cp949",
@@ -94,4 +113,9 @@ if __name__ == "__main__":
     for i in range(len(df)):  # extract needed part
         df.at[i] = df.at[i][5:8]
 
-""" I have to add a function to make can images """
+    make_can_image(df)
+
+    # use this code later to load can image
+    # img = Image.open('filename.png').convert('L')
+    # img_array = np.array(img)
+    # print(img_array)
--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 Reference paper : GIDS: GAN based Intrusion Detection System for In-vehicle Network
 
 You can find dataset in the reference paper.
+
+if you want to use this code, follow under code
+
+1. Download dataset from reference paper
+2. Open Converting_CAN_to_image.py and run it
+3. Then, you can get CAN_Images in CAN_image_datset
+4. (preparing next step. please wait)
